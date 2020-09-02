907b8db97bd16bb571ed9652ffd0f6c9d11c86b8
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-09-02 13:43:10 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,31 @@
+def greeting
+  puts "Hello World"
+end
+
+def say_greeting_five_times
+greeting
+greeting
+greeting
+greeting
+greeting
+end
+
+
+say_greeting_five_times
+
+
+def greeting_a_person(name)
+  puts "Hello #{name}"
+end
+
+greeting_a_person("Maria")
+
+
+def a_method(a, b)
+  puts "hi"
+  a + b --> # what if I use puts?
+end
+ 
+a_method(1,2)
+
+
